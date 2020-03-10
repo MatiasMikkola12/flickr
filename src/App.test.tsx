5d92c86402872f4c9ae3030389d28bb7ef6263f3
Matias Mikkola <matias.mikkola@sainsburys.co.uk>
2020-03-10 13:50:41 +0000

    Initial commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('snapshot of the contents of the card', () => {
+  const theapp = renderer.create(<App />).toJSON();
+  expect(theapp).toMatchSnapshot();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,81 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface AppProps {
+}
+
+interface AppState {
+  data: any
+}
+
+interface Item {
+  title: string
+  media: any
+  author: string
+  description: string
+  link: string
+  tags: string;
+}
+
+class App extends React.Component<AppProps, AppState> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      data: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch('data/data.json')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Response not OK');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({ data: data })
+      })
+      .catch((error) => {
+        console.error('Some issue with the fetch:', error);
+      });
+  }
+
+  render() {
+    const items = this.state.data.items
+    return (
+      <div className="data-wrapper">
+        {items && items.map((item: Item) => (
+          <div className="item-wrapper">
+            <div className="contents">
+              <div className="image-wrapper">
+                <img src={item.media.m} />
+              </div>
+              <div className="title-wrapper">
+                <span><a href={item.media.m}>{item.title}</a> by <a href={item.link}>{item.author}</a></span>
+              </div>
+
+              <div className="description-wrapper">
+                <span>Description: {item.description}</span>
+              </div>
+
+              <div className="dropdown-wrapper">
+                <select className="dropdown" id="tags">
+                  <option value="Tag">Tags</option>
+
+                  {item.tags && item.tags.split(" ").map((tag: string) => (
+                    <option value="tag">{tag}</option>
+                  )
+                  )}
+                </select>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    )
+  }
 }
 
 export default App;
